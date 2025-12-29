d489ad2c86f18192c01aa6d7dd40d0a40f0bf4cd
looperproductions-wq <looperproductions@gmail.com>
2025-12-29 13:55:20 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,1 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-  }
-})
+import { defineConfig, loadEnv } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig(({ mode }) => { const env = loadEnv(mode, process.cwd(), ''); return { plugins: [react()] } })
